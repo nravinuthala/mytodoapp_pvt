6d154aaa9cf9df0685144970f52f093b86aebf4e
nravinuthala <37146811+nravinuthala@users.noreply.github.com>
2024-12-26 11:14:33 +0530

    Update ProjectApplication.java

--- src/main/java/spring/project/ProjectApplication.java
+++ src/main/java/spring/project/ProjectApplication.java
@@ -6,15 +6,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        // Check deployment type from system properties
-        String deploymentType = System.getProperty("deployment.type", "embedded");
-        System.out.println("Running with deployment type: " + deploymentType);
-
         SpringApplication.run(ProjectApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ProjectApplication.class);
     }
 }
