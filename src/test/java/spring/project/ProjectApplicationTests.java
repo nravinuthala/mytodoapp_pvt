1bb7acf3810627c4653d91eacf1a63994dc25143
nravinuthala <37146811+nravinuthala@users.noreply.github.com>
2024-12-26 11:18:31 +0530

    Update ProjectApplicationTests.java

--- src/test/java/spring/project/ProjectApplicationTests.java
+++ src/test/java/spring/project/ProjectApplicationTests.java
@@ -2,6 +2,8 @@ package spring.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ContextConfiguration;
+import spring.project.ProjectApplication; 
 
 @SpringBootTest
 @ContextConfiguration(classes = ProjectApplication.class)
