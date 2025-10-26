e43283880d536a9660f506e847df32db3a0db3a9
nravinuthala <37146811+nravinuthala@users.noreply.github.com>
2025-10-26 20:20:22 +0530

    Update ProjectApplication.java

--- src/main/java/spring/project/ProjectApplication.java
+++ src/main/java/spring/project/ProjectApplication.java
@@ -2,12 +2,18 @@ package spring.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectApplication.class);
+    }
 }
